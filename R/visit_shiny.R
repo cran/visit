e22b97c4d5eb21283ef4b95e4c4b82d1b9d60353
Chenguang Wang <cwang68@jhmi.edu>
2023-08-09 10:30:02 +0000

    version 2.2

--- R/visit_shiny.R
+++ R/visit_shiny.R
@@ -7,7 +7,7 @@
 #' A web browser will be brought up for users to access the GUI of \code{\link{visit}}.
 #'
 #' @examples
-#' if(interactive()){
+#' \dontrun{
 #' vtShiny()}
 #'
 #' @export
@@ -17,7 +17,7 @@ vtShiny <- function() {
     req.pkgs        <- c("shiny", "xtable", "knitr", "rmarkdown", "pander");
     chk.uninstalled <- sapply(req.pkgs, function(x) {!requireNamespace(x, quietly = TRUE)});
     chk.inx         <- which(chk.uninstalled);
-
+    
     if (0 < length(chk.inx)) {
         msg <- paste("For the GUI to work, please install ",
         ifelse(1 < length(chk.inx), "packages ", "package "),
@@ -28,14 +28,14 @@ vtShiny <- function() {
         sep = "");
         stop(msg, call. = FALSE);
     }
-
-
+    
+    
     appDir <- system.file("shiny", package = "visit")
     if (appDir == "") {
         stop("Could not find Shiny directory. Try re-installing `visit`.",
         call. = FALSE)
     }
-
-
+    
+    
     shiny::runApp(appDir, display.mode = "normal");
 }
